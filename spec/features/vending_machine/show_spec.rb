f480fd031052c39ab28295db2d1c64c3c8ad95ae
Tylor Schafer <tylorschafer@gmail.com>
2019-09-06 09:36:41 -0600

    Add machine show page test

--- spec/features/vending_machine/show_spec.rb
+++ spec/features/vending_machine/show_spec.rb
@@ -4,9 +4,19 @@ describe 'Vending Machine Show Page' do
   before :each do
     @owner = Owner.create!(name: "Tylor")
     @machine = @owner.machines.create!(location: "123 Fake Street")
-
+    @snack_1 = Snack.new(name: "snack", price: 1)
+    @snack_2 = Snack.new(name: "other_snack", price: 2)
+    @machine.snacks << @snack_1
+    @machine.snacks << @snack_2
   end
   it "Shows all snacks associated with that vending machine" do
+    visit machine_path(@machine)
 
+    @snacks.each do |snack|
+      within "#snack-#{snack.id}" do
+        expect(page).to have_content("Name: #{snack.name}")
+        expect(page).to have_content("Price: #{snack.price}")
+      end
+    end
   end
 end
